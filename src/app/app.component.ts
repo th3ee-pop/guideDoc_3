c778435c8df689e23ddd29a5def3bb3b08f0bc8a
th3eesome <610870693@qq.com>
2017-12-19 20:36:18 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  list: any[] = [];
   title = 'app';
+  current = 1;
+
+  ngOnInit() {
+    for (let i = 0; i < 20; i++) {
+      this.list.push({
+        key: i.toString(),
+        title: `content${i + 1}`,
+        description: `description of content${i + 1}`,
+        direction: Math.random() * 2 > 1 ? 'right' : ''
+      });
+    }
+    console.log(this.list);
+  }
+
+  filterOption(inputValue, option) {
+    return option.description.indexOf(inputValue) > -1;
+  }
+
+  search(ret: any) {
+    console.log('nzSearchChange', ret);
+  }
+
+  select(ret: any) {
+    console.log('nzSelectChange', ret);
+  }
+
+  change(ret: any) {
+    console.log('nzChange', ret);
+  }
 }
